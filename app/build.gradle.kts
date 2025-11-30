90c18a51072dfdf9aac97eea98427046d0bc894f
GiannyGarcia <gianneilvingarcia@gmail.com>
2025-12-01 00:27:27 +0800

    Added DB, fixing FAB

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
